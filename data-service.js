da5c023efc880a5a64a7250e8850fd2e60786175
sukhvir-singh3 <sukhvir-singh3@myseneca.ca>
2023-02-17 22:08:13 -0500

    Initial commit

--- data-service.js
+++ data-service.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+
+const upload = multer();
 
 let students = [];
 let programs = [];
+let images   = [];
 
 function initialize(){
 	return new Promise((resolve, reject) => {
@@ -50,9 +55,102 @@ function getPrograms() {
   });
 }
 
+function addImage(imageUrl){
+	return new Promise((resolve, reject)=>{
+		cloudinary.uploader.upload(imageUrl, { folder: "web_as3" }, (error, result)=>{
+			if(error){
+				console.log(error);
+				reject(error);
+			}
+			else{
+				console.log(result);
+				images.push(imageUrl);	
+				//resolve(result.secure_url);
+				resolve(imageUrl);
+		
+			}
+		});
+	});
+}
+
+function getImages(){
+	return new Promise((resolve, reject)=>{
+		if(images.length == 0)
+			reject('no results returned');		
+		else{
+			resolve(images);
+		}
+	});
+}
+
+function addStudent(studentData){
+	return new Promise((resolve, reject)=>{
+		if(studentData.isInternationalStudent === undefined)				
+			studentData.isInternationalStudent = false;
+		else
+			studentData.isInternationalStudent = true;
+		var largest = 0;
+		students.forEach((student)=>{
+			if(largest > parseInt(student.studentID))	
+				largest = parseInt(student.studentID);
+		});
+		largest++;
+		studentData.studentID = largest.toString();
+		students.push(studentData);
+		resolve(studentData);
+	});
+}
+
+function getStudentsByStatus(status){
+	return new Promise((resolve, reject)=>{
+		const matchedStudents = students.filter(student => student.status === status);
+		if(matchedStudents.length === 0)
+			reject('no result found');
+		else
+			resolve(matchedStudents);
+	});
+}
+
+function getStudentsByProgramCode(programCode){
+	return new Promise((resolve, reject)=>{
+		const matchedStudents = students.filter(student => student.program === programCode);
+		if(matchedStudents.length === 0)
+			reject('no result found');
+		else
+			resolve(matchedStudents);
+	});
+}
+
+function getStudentsByExpectedCredential(credential){
+	return new Promise((resolve, reject)=>{
+		const matchedStudents = students.filter(student => student.expectedCredential === credential);
+		if(matchedStudents.length === 0)
+			reject('no result found');
+		else
+			resolve(matchedStudents);
+	});
+}
+
+function getStudentById(sid){
+	return new Promise((resolve, reject)=>{
+		const matchedStudents = students.filter(student => student.studentID === sid);
+		if(matchedStudents.length === 0)
+			reject('no result found');
+		else
+			resolve(matchedStudents[0]);
+	});
+}
+
 module.exports = {
   initialize,
   getAllStudents,
   getInternationalStudents,
-  getPrograms
+  getPrograms,
+  addImage,
+  getImages,
+  addStudent,
+  getStudentsByStatus,
+  getStudentsByProgramCode,
+  getStudentsByExpectedCredential,
+  getStudentById
 };
--- server.js
+++ server.js
@@ -17,8 +17,20 @@ var express = require('express');
 var path = require('path');
 var dataService = require('./data-service.js');
 const fs = require('fs');
+/**********************AS2***********************/
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+cloudinary.config({
+  cloud_name: 'dp0cdwjfg',
+  api_key: '464917171124998',
+  api_secret: 'CrLvMl5Hb2qtk4iPskc-pKVVFUU',
+  secure: true
+});
 
 var app = express();
+const upload = multer();
 
 app.use(express.static('public'));
 
@@ -38,15 +50,152 @@ app.get('/about', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/about.html'))
 });
 
+/**********************AS2***********************/
+app.use(express.urlencoded({ extended: true }));
+
+//Add Studnet
+app.get('/students/add', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/addStudent.html'))
+});
+
+//Add Images
+app.get('/images/add', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/addImage.html'))
+});
+
+//Request for image
+app.post('/images/add',upload.single('imageFile'), (req,res)=>{
+	 if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processForm(uploaded.url);
+        });
+    }else{
+        processForm("");
+    }
+
+    function processForm(imageUrl){
+
+        // TODO: Process the image url on Cloudinary before redirecting to /images.
+        // Note: the required "addImage" function is not created yet in data-service.js.
+        // ... ...
+         dataService.addImage(imageUrl)
+	    .then(() => {
+     		 res.redirect("/images");
+    		})
+    		.catch((err) => {
+    		  console.log(err);
+     		 res.redirect("/images");
+    		});
+     }
+});
+
+
+app.get('/images', (req, res)=>{
+	dataService.getImages()
+		.then((images)=>{
+			res.json({images: images});	
+		})
+		.catch((err)=>{
+			res.status(500).json({error: err});
+ 		});
+});
+
+//for sutdents POST /students/add
+app.post('/students/add', (req, res)=>{
+	dataService.addStudent(req.body)
+		.then(()=>{
+			res.redirect('/students');
+		})	
+    		.catch((error) => {
+      		console.error(error);
+     			 res.sendStatus(500);
+    		});
+});
+app.get('/student/:id', (req, res) => {
+	  const studentId = req.params.id;
+
+  	dataService.getStudentById(studentId)
+    		.then((student) => {
+     			 if (student) {
+      			  res.json(student);
+     			 } else {
+    			    res.status(404).json({ message: 'Student not found' });
+     			 }
+    		})
+   		 .catch((err) => {
+  	    res.status(500).json({ message: err });
+    });
+});
 //Students File
+//NOTE: I was trying somethign new so i put the them in comments, becasue i want to match the output of the 
+//      assignment as required which i did ^^ 
 app.get('/students', (req, res) => {
-	dataService.getAllStudents()
-		.then((studentsArr) => {
-			//var studnet = studentsArr.map((elem, index) => (index + 1) + '. ' + elem.firstName + '<br>');
-			//var namesWithoutCommas = studnet.join('');
-		    	//res.send(`Student Names:<br>  ${namesWithoutCommas}`);
-		    res.send(studentsArr);
-		}).catch((err)=>{res.json({ message: err });});
+	// if there are no query parameters
+	if(Object.keys(req.query).length === 0){
+		dataService.getAllStudents()
+			.then((studentsArr) => {
+		  	  res.send(studentsArr);
+			})
+			.catch((err)=>{
+				res.json({ message: err });
+		 	 });
+	}
+	// if there are query parameters
+	else{
+		let status = req.query.status;	
+		let program = req.query.program;	
+		let credential = req.query.credential;	
+		
+		if(status){
+			dataService.getStudentsByStatus(status)
+				.then((studentsArr)=>{
+					res.json(studentsArr);
+				})
+				.catch((err)=>{
+					res.json({ message: err });
+				});
+		}
+		else if(program){
+			dataService.getStudentsByProgramCode(program)
+				.then((studentsArr)=>{
+					res.json(studentsArr);
+				})
+				.catch((err)=>{
+					res.json({ message: err });
+				});
+		}
+		else if(credential){
+			dataService.getStudentsByExpectedCredential(credential)
+				.then((studentsArr)=>{
+					res.json(studentsArr);
+				})
+				.catch((err)=>{
+					res.json({ message: err });
+				});
+		}
+	}
 });
 
 //Intenational student File
@@ -75,7 +224,7 @@ app.get('/programs', (req, res)=>{
 });
 
 app.use((req,res)=>{
-	res.status(404),send('Page Not Found');
+	res.status(404).send('Page Not Found');
 })
 dataService.initialize()
 	.then(() => {
